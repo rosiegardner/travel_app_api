550c9fce7165f99a9acb5026c7965acf564b012e
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-21 20:10:24 -0700

    show review by id

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,11 @@ class ReviewsController < ApplicationController
     json_response(@reviews)
   end
 
+  def show
+    @review = Review.find(params[:id])
+    json_response(@review)
+  end
+
   private
   def json_response(object, status = :ok)
     render json: object, status: status
