e1b0d4cf4bb3e729fe59a75da5776bfbef81b429
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-22 19:02:40 -0700

    will return a created status - expect fail

--- spec/requests/post_route_spec.rb
+++ spec/requests/post_route_spec.rb
@@ -13,4 +13,8 @@ describe "post a review route", :type => :request do
   it 'returns the review content' do 
     expect(JSON.parse(response.body)['content']).to eq('test_content')
   end
+
+  it 'returns a created status' do
+    expect(response).to have_http_status(:created)
+  end
 end
