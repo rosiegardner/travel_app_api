61a90568baa70e7eac4bdd666dca33722cc8c88c
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-22 20:11:05 -0700

    update a authors name - passes test

---
+++ spec/requests/put_route_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'update a review route', :type => :request do
+
+  before do
+    post '/reviews', params: { :author => 'test_author', :content => 'test_content' }
+    @review_id = JSON.parse(response.body)['id']
+    patch "/reviews/#{@review_id}" , params: { :author => 'testie_author', :content => 'testie_content' }
+    get "/reviews/#{@review_id}"
+  end
+
+  it 'updates the authors name' do
+    puts(response.body)
+    expect(JSON.parse(response.body)['author']).to eq('testie_author')
+  end
+end
