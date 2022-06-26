d278c402118f30a1c1bd4ba8f68a8e5f5fd0aacc
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-26 11:11:25 -0700

    return a RecordInvalid error - expect fail

--- spec/requests/exceptions_spec.rb
+++ spec/requests/exceptions_spec.rb
@@ -8,3 +8,14 @@ describe 'get a review route with exception handler', :type => :request do
     expect(response).to have_http_status(:not_found)
   end
 end
+
+describe 'post a review route with exception handlers', :type => :request do
+
+  before do
+    post '/reviews', params: { :author => "test_author", :content => nil }
+  end
+
+  it 'returns a RecordInvalid error' do
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+end
