51c4b95a606eaf30e16221f485bdb05cb9133d61
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-26 15:24:01 -0700

    add search method for author

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord  
   validates :author, presence: true
   validates :content, presence: true
+
+  def self.search(search)
+    if search
+      where(["author ILIKE ?","%#{search}%"])
+    else
+      all
+    end
+  end
 end
